e9c2f3cf36a5248b0a84fdbc63a6460db72e0355
Justin V. <vietzi99@gmail.com>
2024-09-21 18:32:36 +0200

    feat: disable work page for now

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -5,7 +5,7 @@
 <nav class="container mx-auto px-4 py-5 border-b border-black">
 	<ul class="flex gap-3">
 		<li><a class:active={$page.url.pathname === '/'} href="/">home</a></li>
-		<li><a class:active={$page.url.pathname === '/work'} href="/work">work</a></li>
+		<!-- <li><a class:active={$page.url.pathname === '/work'} href="/work">work</a></li> -->
 	</ul>
 </nav>
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
 	import Navbar from '$lib/Navbar.svelte';
-import '../app.css';
+	import '../app.css';
 
-let year = new Date().getFullYear();
+	let year = new Date().getFullYear();
 </script>
 
 <Navbar />
@@ -12,10 +12,16 @@ let year = new Date().getFullYear();
 </div>
 
 <footer class="container mx-auto px-4 border-t border-black pt-3 mt-10">
-  <ul class="flex flex-col gap-1">
-    <li><a class="underline" target="_blank" href="https://github.com/kiesman99">GitHub</a></li>
-    <li><a class="underline" target="_blank" href="https://github.com/kiesman99">X (Twitter)</a></li>
-    <li><a class="underline" target="_blank" href="https://www.linkedin.com/in/justin-vietz">Linkedin</a></li>
-  </ul>
-  <p class="mt-10">© {year} Justin Vietz. All rights reserved.</p>
+	<ul class="flex flex-col gap-1">
+		<li><a class="underline" target="_blank" href="https://github.com/kiesman99">GitHub</a></li>
+		<!-- <li>
+			<a class="underline" target="_blank" href="https://github.com/kiesman99">X (Twitter)</a>
+		</li> -->
+		<li>
+			<a class="underline" target="_blank" href="https://www.linkedin.com/in/justin-vietz"
+				>Linkedin</a
+			>
+		</li>
+	</ul>
+	<p class="mt-10">© {year} Justin Vietz. All rights reserved.</p>
 </footer>
